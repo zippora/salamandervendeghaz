390484c3f62129a4baad32cc0bedbc61ab4b9cae
Zippora Levering <zippora.levering@duo.nl>
2019-10-25 14:44:42 +0200

    header wat verbeterd

--- src/App.js
+++ src/App.js
@@ -156,7 +156,7 @@ export class App extends Component {
                     <div className="App">
                         <div className="App-header">
                             <div className="container">
-                                <img className="App-logo" src="./img/logokrisztina.jpg" alt="logo"/>
+                                <img className="App-logo" src="./img/logo.png" alt="logo"/>
 
                                 <h2>Salamander Vendégház</h2>
                                 <nav>
